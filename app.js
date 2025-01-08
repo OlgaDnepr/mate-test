6c78a500e85ba4bbfbffd7d12df02982345063d1
Olga <cocochoco.story@gmail.com>
2025-01-08 11:24:08 +0200

    Ternary vs If Else

--- app.js
+++ app.js
@@ -1,4 +1,28 @@
-'use strict'; <Shift+Enter for a newline>
+'use strict'; 
 
-let age = 20
-console.log(age);
+const age = 11;
+let message2 = '';
+
+if (age < 7) {
+  message2 = 'baby';
+} else if (age < 15) {
+  message2 = 'tin';
+} else if (age < 18) {
+  message2 = 'Child';
+} else {
+  message2 = 'Adult';
+}
+console.log(message2);
+
+
+const message = (age < 7) 
+  ? 'baby'
+  : (age < 15)
+    ? 'tin'
+    : (age < 18)
+      ? 'Child'
+      : 'Adult';
+console.log(message);
+
+
+
