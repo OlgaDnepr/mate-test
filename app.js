f61e2f06669b93be79627a38641bf66ae574648f
Olga <cocochoco.story@gmail.com>
2025-01-10 20:00:59 +0200

    добавили getCentury

--- app.js
+++ app.js
@@ -1,41 +1,49 @@
 'use strict'; 
 
-const age = 11;
-let message2 = '';
+// const age = 11;
+// let message2 = '';
 
-if (age < 7) {
-  message2 = 'baby';
-} else if (age < 15) {
-  message2 = 'tin';
-} else if (age < 18) {
-  message2 = 'Child';
-} else {
-  message2 = 'Adult';
+// if (age < 7) {
+//   message2 = 'baby';
+// } else if (age < 15) {
+//   message2 = 'tin';
+// } else if (age < 18) {
+//   message2 = 'Child';
+// } else {
+//   message2 = 'Adult';
+// }
+// console.log(message2);
+
+
+// const message = (age < 7) 
+//   ? 'baby'
+//   : (age < 15)
+//     ? 'tin'
+//     : (age < 18)
+//       ? 'Child'
+//       : 'Adult';
+// console.log(message);
+
+// let number = 0;
+// number++;
+// ++number;
+// console.log(number);
+
+// function(x) {
+//   return x++
+// }
+
+// const y = Increment (2);
+// console.log(y);
+
+
+function getCentury(year) {
+  if (year === 0) {
+  return 1;
+  }
+  return Math.ceil(year / 100)
 }
-console.log(message2);
-
-
-const message = (age < 7) 
-  ? 'baby'
-  : (age < 15)
-    ? 'tin'
-    : (age < 18)
-      ? 'Child'
-      : 'Adult';
-console.log(message);
-
-let number = 0;
-number++;
-++number;
-console.log(number);
-
-function(x) {
-  return x++
-}
-
-const y = Increment (2);
-console.log(y);
-
-
+const result = getCentury(1876);
+console.log(result);
 
 
