0debb01c67053b94830899fa8c12e90caf6700de
Olga <cocochoco.story@gmail.com>
2025-01-16 22:28:03 +0200

    continue, разбив рядка на массив по два символа

--- app.js
+++ app.js
@@ -81,7 +81,7 @@ console.log(typeof myNumber1); //проверяем какой тип данны
 const ss = 5;
 let sum1 = 0;
 
-debugger;
+
 
 for (let i = 1; i <= ss; i++) {
   sum1 += i;
@@ -89,3 +89,93 @@ for (let i = 1; i <= ss; i++) {
   
 }
 console.log(sum1);
+
+// debugger;
+
+
+function calculateProfit(amount, percent, period) {
+  let sum = amount;
+ 
+  for (let i = 1; i <= period; i++) {
+    sum += sum * (percent / 100);
+    console.log(sum);
+  }
+  return sum;
+}
+let ttt = calculateProfit(1000, 5.5, 7);
+console.log(ttt);
+
+
+function splitString(str) {
+  let length = str.length % 2;
+  let pairing = str;
+  
+  if (length !== 0) {
+     pairing = `${str}_`;
+  } 
+  console.log(pairing);
+  let pr = '';
+  let res = [];
+  for (let i = 0; i < pairing.length; i = i + 2) {
+    // pairing.split()
+    pr = pairing[i] + pairing[i + 1];
+    res.push(pr);
+  }
+  return res;
+}
+let result2 = splitString('gvfg hsfc');
+console.log(result2);
+
+let i = 2
+for (; i > 1; i++) {
+  if (i === 9) {
+    break;
+  }
+
+}
+console.log(i);
+
+
+for (let i = 2; i > 1; i++) {
+  if (i === 9) {
+    break;
+  }
+
+  console.log(i);
+}
+
+
+for (let i = 20; i <= 30; i++) {
+  if (i === 25) {
+    continue;
+  }
+
+  console.log(i);
+}
+function name(qqq) {
+  
+  let result = 0;
+  for (let i = 1; i <= qqq; i++) {
+    if (i === 5) {
+      continue;
+    }
+    result += i;
+    console.log(result);
+    
+  }
+  return result;
+}
+let rrr = name(22);
+console.log(rrr);
+ 
+// debugger
+let result22 = 0;
+for (let i = 1; i <= 7; i++) {
+  if (i === 5) {
+    continue;
+  }
+  result22 += i;
+  console.log(i);
+  console.log(result22);
+  
+}
